f61aa8dd873d6eaea5d3c3f7022d279c996540ba
Jens Papenhagen <jenspapenhagen@users.noreply.github.com>
2019-02-09 11:53:36 +0100

    adding doc and loadlib.
    adding doc and load the owning JNI cpp lib. here

--- src/main/java/eu/papenhagen/jniplay/MainApp.java
+++ src/main/java/eu/papenhagen/jniplay/MainApp.java
@@ -1,13 +1,17 @@
 package eu.papenhagen.jniplay;
 
 /**
- * playing with JNI
+ * use basic JNI
+ * with own code (c++) and header File
+ * to generate the header File use the maven profile
+ * this is only to out soruce code in c++ (maybe for speed)
  *
  * @author jens.papenhagen
  */
 public class MainApp {
 
     public static void main(String[] args) {
+        System.loadLibrary("eu_papenhagen_jniplay_MainApp");
         output();
     }
 
